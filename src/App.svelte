c6296aa7a74907ad2cec8ea579180ae643f1187f
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2021-03-01 00:07:57 +0200

    added MeetupGrid and MeetupItem

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,36 @@
 <script>
-	import Header from "./UI/Header.svelte";
-  </script>
-  
-  <Header />
-  
+	import Header from './UI/Header.svelte';
+	import MeetupGrid from './Meetups/MeetupGrid.svelte';
+
+	const meetups = [
+		{
+			id: 'm1',
+			title: 'Coding Bootcamp',
+			subtitle: 'Learn to code in 2 hours',
+			description: 'dasfasfas faasf ',
+			imageUrl: 'https://i.imgur.com/lKJiT77.png',
+			address: '27th Nerd Road, 4, 24315 New York',
+			contactEmail: 'code@test.com'
+		},
+		{
+			id: 'm2',
+			title: 'Swim Together',
+			subtitle: 'Let\'s have some swimming',
+			description: '122 swim',
+			imageUrl: 'https://i.imgur.com/oEUksmz.png',
+			address: '29th Nerd Road, 4, 24315 New York',
+			contactEmail: 'code1@test.com'
+		}
+	]
+</script>
+
+<style>
+	#meetups {
+		margin-top: 5rem;
+	}
+</style>
+
+<Header />
+
+<MeetupGrid 
+	{meetups}/>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,31 @@
+<script>
+    import MeetupItem from './MeetupItem.svelte';
+    export let meetups;
+</script>
+
+<style>
+	section {
+        width: 100%;
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 1rem;
+    }
+
+    @media (min-width: 758px) {
+        section {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
+
+<section >
+	{#each meetups as meetup}
+		<MeetupItem 
+			title={meetup.title}
+			subtitle={meetup.subtitle}
+			imageUrl={meetup.imageUrl}
+			description={meetup.description}
+			address={meetup.address}
+			email={meetup.email} />
+	{/each}
+</section>
